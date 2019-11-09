5ded7e9115ff5b9543414eada1a459d24e755fd1
Curtis <curtishilgenberg@gmail.com>
2019-11-09 13:45:52 -0700

    mysteriously vaguely working configuration

--- main.js
+++ main.js
@@ -2,15 +2,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Murmur_1 = require("./Murmur");
 const ice_1 = require("ice");
-let ic = ice_1.Ice.initialize();
+let murmur;
 ice_1.Ice.Promise.try(function () {
+    let ic = ice_1.Ice.initialize();
     let base = ic.stringToProxy("Meta:tcp -h 127.0.0.1 -p 6502");
-    Murmur_1.Murmur.MetaPrx.checkedCast(base);
-}).finally(function () {
-    if (ic) {
-        return ic.destroy();
-    }
-}).catch(function (ex) {
-    console.log(ex.toString());
-    process.exit(1);
+    Murmur_1.Murmur.MetaPrx.checkedCast(base).then((metaprx) => (metaprx.getAllServers().then((serverList) => {
+        serverList.forEach((server) => {
+            server.getUsers().then((users) => {
+                users.forEach((user) => {
+                    console.log(user.name);
+                });
+            });
+        });
+    }))).catch((e) => {
+        ic.destroy();
+    }).finally(function () {
+    }).catch(function (ex) {
+        //console.log(ex.toString());
+        process.exit(1);
+    });
 });
--- main.ts
+++ main.ts
@@ -2,25 +2,42 @@ import { Murmur } from "./Murmur";
 import { Ice } from "ice";
 
 
-let ic = Ice.initialize();
+
+let murmur: any;
 
 Ice.Promise.try(
     function()
     {
+        let ic = Ice.initialize();
         let base = ic.stringToProxy("Meta:tcp -h 127.0.0.1 -p 6502");
-        Murmur.MetaPrx.checkedCast(base);
+        Murmur.MetaPrx.checkedCast(base).then( 
+            (metaprx) => (metaprx.getAllServers().then (
+                (serverList) => {
+                    serverList.forEach((server) =>{
+                        server.getUsers().then(
+                            (users) => {
+                                users.forEach(
+                                    (user) => {
+                                        console.log(user.name);
+                                    }
+                                )
+                            }
+                        )
+                    })
+                }
+            ))
+            ).catch((e) => {//console.log(e);});}}
+        ic.destroy();
     }
 ).finally(
     function()
     {
-        if(ic)
-        {
-            return ic.destroy();
-        }
+
+        
     }
 ).catch(
     function(ex)
     {
-        console.log(ex.toString());
+        //console.log(ex.toString());
         process.exit(1);
     });
